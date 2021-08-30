992d9fd350dcd7097f46365e4e24ce0dabc92c2c
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-08-30 14:35:54 -0400

    added files

---
+++ shouldIFF.py
@@ -0,0 +1,98 @@
+from riotwatcher import LolWatcher
+import numpy as np
+
+watcher = LolWatcher('RGAPI-741e14e0-ea7e-4439-a05a-ad4e4bc1bdf2')
+
+class leagueStats:
+    def winrate(summonerName):
+        summoner = watcher.summoner.by_name('na1',summonerName)
+        stats = watcher.league.by_summoner('na1',summoner['id'])
+        return round(100*(stats[0]['wins']/(stats[0]['wins']+stats[0]['losses'])))
+# function for a player that forfeits every game predetermined to be a loss at 20 minutes
+    def runSimForfeit(winrate,gain,lose):
+        x = []
+        y = []
+        time = 0
+        lp = 0
+# start a loop that ends when the player hits 100 hours
+        while time < 6000:
+# test if game is predetermined to be a loss
+            if winrate < np.random.randint(1,101):
+                lp -= lose
+                time += 20
+# else game is predetermined to be a win
+            else:
+                lp += gain
+                time += np.random.randint(20,56)
+# store values to an array
+            x.append(time)
+            y.append(lp)
+        return [x,y]
+# function for a player that holds every game hostage for the chance that a predetermined loss is winnable
+    def runSimHostage(winrate,winnablePercent,gain,lose):
+        x = []
+        y = []
+        time = 0
+        lp = 0
+# start a loop that ends when the player hits 100 hours
+        while time < 6000:
+# test if the game is predetermined to be a loss
+            if winrate < np.random.randint(1,101):
+# test if the predetermined loss is actually a loss
+                if winnablePercent < np.random.randint(1,101):
+                    lp -= lose
+                    time += 40
+# else game is won
+                else:
+                    lp += gain
+                    time += 40
+# else game is predetermined to be a win
+            else:
+                lp += gain
+                time += np.random.randint(20,56)
+# store values to an array
+            x.append(time)
+            y.append(lp)
+        return [x,y]
+    def main():
+# prompt user input
+        summonerName = input('Summoner Name? ')
+        gain = int(input('What are your LP gains? '))
+        lose = int(input('What are your LP losses? '))
+        winrate = leagueStats.winrate(summonerName)
+# run a simulation i times and plot each point generated
+        totalForfeit = [[],[]]
+        totalHostage = [[],[]]
+        graphsForfeit = []
+        graphsHostage = []
+        maxForfeit = 0
+        maxHostage = 0
+        winnablePercent = 0
+        count = 0
+        print('Simulating games')
+        while maxForfeit >= maxHostage:
+            totalForfeit = [[],[]]
+            totalHostage = [[],[]]
+            for i in range(100):
+                tempForfeit = leagueStats.runSimForfeit(winrate,gain,lose)
+                tempHostage = leagueStats.runSimHostage(winrate,winnablePercent,gain,lose)
+# store values for line of best fit
+                for j in range(2):
+                    totalForfeit[j] += tempForfeit[j]
+                    totalHostage[j] += tempHostage[j]
+# create and draw lines of best fit
+            zForfeit = np.polyfit(totalForfeit[0],totalForfeit[1],1)
+            zHostage = np.polyfit(totalHostage[0],totalHostage[1],1)
+            pForfeit = np.poly1d(zForfeit)
+            pHostage = np.poly1d(zHostage)
+            maxForfeit = pForfeit(6000)
+            maxHostage = pHostage(6000)
+            winnablePercent += 1
+            graphsForfeit.append(pForfeit)
+            graphsHostage.append(pHostage)
+            count += 1
+            print('Simulation {count}:'.format(count=count))
+            print('LP forfeit: '+str(maxForfeit))
+            print('LP hostage: '+str(maxHostage))
+        print('{totalHours} hours were simulated to determine that you should FF unless your game is winnable AT LEAST {winnablePercent}% of the time'.format(totalHours=count*100,winnablePercent=winnablePercent-1))
+leagueStats.main()
